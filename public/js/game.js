93cf1200312fc18d601bef68389540cb268a83de
arty4999 <49936054+arty4999@users.noreply.github.com>
2020-05-30 02:27:04 +0800

    write functions for game

---
+++ public/js/game.js
@@ -0,0 +1,46 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'Phasers',
+    width: 1920,
+    height: 720,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: { y: 0}
+    }
+},
+scene: {
+    preload: preload,
+    create: create,
+    update: update
+}
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('ship', 'assets/spaceShips_001.png');
+    this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+    this.load.image('star', 'assets/star_gold.png');
+}
+
+function create() {
+    var self = this;
+    this.socket = io();
+    this.otherPlayers = this.physics.add.group();
+    this.socket.on('currentPlayers', function (players) {
+        Object.keys(players).forEach(function (id) {
+            if (players[id].playerId === self.socket.id) {
+                addPlayer(self, players[id]);
+            } else {
+                addOtherPlayers(self, players[id]);
+            }
+        });
+    });
+    this.socket.on('newPlayer', function(playerInfo) {
+        addOtherPlayers(self,playerInfo);
+    });
+
+
+}
