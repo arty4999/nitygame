9f223a2cdba5c2c26626884b24abb1f590246f25
arty4999 <49936054+arty4999@users.noreply.github.com>
2020-05-30 03:44:19 +0800

    complete game.js

--- public/js/game.js
+++ public/js/game.js
@@ -47,7 +47,7 @@ function create() {
         otherPlayer.destroy();
       }
     });
-  }); 
+  });
   this.socket.on("playerMoved", function (playerInfo) {
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
       if (playerId === otherPlayer.playerId) {
@@ -65,18 +65,100 @@ function create() {
   this.artyScoreText = this.add.text(584, 16, "", {
     fontSize: "32px",
     fill: "#FF0000",
-  });//
-
-  this.socket.on('scoreUpdate', function(scores){
-      self.nityScoreText.setText('Nity:' + scores.nity);
-      self.artyScoreText.setText('Arty:' + scores.arty);
   });
 
-  this.socket.on('starLocation', function(starLocation){
-      if (self.star) self.star.destroy();
-      self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
-      self.physics.add.overlap(self.ship, self.star, function() {
-          this.socket.emit('starCollected');
-      }, null, self);
+  this.socket.on("scoreUpdate", function (scores) {
+    self.nityScoreText.setText("Nity:" + scores.nity);
+    self.artyScoreText.setText("Arty:" + scores.arty);
   });
+
+  this.socket.on("starLocation", function (starLocation) {
+    if (self.star) self.star.destroy();
+    self.star = self.physics.add.image(starLocation.x, starLocation.y, "star");
+    self.physics.add.overlap(
+      self.ship,
+      self.star,
+      function () {
+        this.socket.emit("starCollected");
+      },
+      null,
+      self
+    );
+  });
+}
+
+function addPlayer(self, playerInfo) {
+  SVGAnimatedLength.ship = self.physics.add
+    .image(playerInfo.x, playerInfo.y, "ship")
+    .setOrgin(0.5, 0.5)
+    .setDisplaySize(53, 40);
+  if (playerInfo.team === "Nity") {
+    self.ship.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
+}
+
+function addOtherPlayers(self, playerInfo) {
+  const otherPlayer = seld.add
+    .sprite(playerInfo.x, playerInfo.y, "otherPlayer")
+    .setOrgin(0.5, 0.5)
+    .setDisplaySize(53, 40);
+  if (playerInfo.team === "Nity") {
+    otherPlayer.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayer.add(otherPlayer);
+}
+
+function update() {
+  if (this.ship) {
+    if (this.cursors.left.isDown) {
+      this.ship.setAngularVelocity(-150);
+    } else if (this.cursors.right.isDown) {
+      this.ship.setAngularVelocity(150);
+    } else {
+      this.ship.setAngularVelocity(0);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.physics.velocityFromRotation(
+        this.ship.rotation + 1.5,
+        100,
+        this.ship.body.acceleration
+      );
+    } else {
+      tjis.ship.setAcceleration(0);
+    }
+
+    this.physics.world.wrap(this.ship, 5);
+
+    //emit player movement
+    var x = this.ship.x;
+    var y = this.ship.y;
+    var r = this.ship.rotation;
+    if (
+      this.ship.oldPosition &&
+      (x !== this.ship.oldPosition.x ||
+        y !== this.ship.oldPosition.y ||
+        r !== this.ship.oldPosition.rotation)
+    ) {
+      this.socket.emit("playerMovement", {
+        x: this.ship.x,
+        y: this.ship.y,
+        rotation: this.ship.rotation,
+      });
+    }
+    //save old pos data
+    this.ship.oldPosition = {
+      x: this.ship.x,
+      y: this.ship.y,
+      rotation: this.ship.rotation,
+    };
+  }
 }
