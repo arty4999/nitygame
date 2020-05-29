0a5809098fbb376fb5d67113ed5cd68fff94c469
arty4999 <49936054+arty4999@users.noreply.github.com>
2020-05-30 02:31:49 +0800

    format files to look nicer

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
-var express = require('express'); // make's ExpressJS a dependency
+var express = require("express"); // make's ExpressJS a dependency
 var app = express(); // app launches a expressVM
-var server = require('https').Server(app); // Http Web server started
-var io = require('socket.io').listen(server); // Listen for connection to port
+var server = require("https").Server(app); // Http Web server started
+var io = require("socket.io").listen(server); // Listen for connection to port
 
 var players = {};
 var star = {
-    x: Math.floor(Math.random() * 700) + 50,
-    y: Math.floor(Math.random() * 500) + 50
+  x: Math.floor(Math.random() * 700) + 50,
+  y: Math.floor(Math.random() * 500) + 50,
 };
 var scores = {
-    Nity: 0,
-    Arty: 0
+  Nity: 0,
+  Arty: 0,
 };
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-    console.log('New Gay man', socket.id);
-    //Create New player and add to player object
-    players[socket.id] = {
-        rotation: 0,
-        x: Math.floor(Math.random() * 700) +50,
-        y: Math.floor(Math.random() * 500) +50,
-        playerId: socket.id,
-        team: (Math.floor(Math.random() * 2) == 0) ? 'Nity' : 'Arty'
-    };
-    // send player object to new player
-    socket.emit('currentPlayers', players);
-    // send starts to new player
-    socket.emit('starlocation', star);
-    // send current scores
-    socket.emit('scoreUpdate', scores);
-    //update all players of new player
-    socket.emit('newPlayer', players[socket.id]);
+io.on("connection", function (socket) {
+  console.log("New Gay man", socket.id);
+  //Create New player and add to player object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: Math.floor(Math.random() * 2) == 0 ? "Nity" : "Arty",
+  };
+  // send player object to new player
+  socket.emit("currentPlayers", players);
+  // send starts to new player
+  socket.emit("starlocation", star);
+  // send current scores
+  socket.emit("scoreUpdate", scores);
+  //update all players of new player
+  socket.emit("newPlayer", players[socket.id]);
 
-    // When a player disconnects, remove from player object
-    socket.on('disconnect', function() {
-        console.log('Gay gone:', socket.id);
-        delete players[socket.id];
-        // emit all players to remove the disconnected
-        io.emit('disconnect', socket.id);
-    }); // part 1 ended here
+  // When a player disconnects, remove from player object
+  socket.on("disconnect", function () {
+    console.log("Gay gone:", socket.id);
+    delete players[socket.id];
+    // emit all players to remove the disconnected
+    io.emit("disconnect", socket.id);
+  }); // part 1 ended here
 
   // when a player moves, update the player data
-  socket.on('playerMovement', function (movementData) {
+  socket.on("playerMovement", function (movementData) {
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
     players[socket.id].rotation = movementData.rotation;
     // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
+    socket.broadcast.emit("playerMoved", players[socket.id]);
   });
 
-    socket.on('starCollected', function() {
-        if (players[socket.id].team === 'Nity') {
-            scores.Nity += 10;
-        } else {
-            scores.Arty +10;
-        }
+  socket.on("starCollected", function () {
+    if (players[socket.id].team === "Nity") {
+      scores.Nity += 10;
+    } else {
+      scores.Arty + 10;
+    }
     star.x = Math.floor(Math.random() * 700) + 50;
     star.y = Math.floor(Math.random() * 500) + 50;
-    io.emit('starlocation', star);
-    io.emit('scoreUpdate', scores);
-    });
+    io.emit("starlocation", star);
+    io.emit("scoreUpdate", scores);
+  });
 });
 
 server.listen(8081, function () {
-    console.log(`Listening on ${server.address().port}`);
-  });  
+  console.log(`Listening on ${server.address().port}`);
+});
